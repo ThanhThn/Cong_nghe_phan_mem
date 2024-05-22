22aee3642c8827c8602a5340b061625b2c39c728
Lê Hoàng Quốc Thanh <lhqthanh1809@gmail.com>
2024-05-22 12:07:15 +0700

    merge

--- Software/obj/Debug/net7.0/Software.AssemblyInfo.cs
+++ Software/obj/Debug/net7.0/Software.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Software")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4183b62c21092d98ac0548e5a7b21f8ecd28699f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Software")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Software")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
