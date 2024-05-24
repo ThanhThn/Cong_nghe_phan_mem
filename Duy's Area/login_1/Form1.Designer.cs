67d2de037da2302e44120dfb013b048163d3ef6d
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-05-24 12:21:52 +0700

    update

--- Duy's Area/login_1/Form1.Designer.cs
+++ Duy's Area/login_1/Form1.Designer.cs
@@ -30,11 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.readyLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
             this.userNameTxt = new System.Windows.Forms.TextBox();
             this.passwordTxt = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
             this.signInTxt = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -49,27 +49,27 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // label1
+            // readyLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.White;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
-            this.label1.Location = new System.Drawing.Point(534, 89);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Ready to Login";
+            this.readyLabel.AutoSize = true;
+            this.readyLabel.BackColor = System.Drawing.Color.White;
+            this.readyLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.readyLabel.ForeColor = System.Drawing.Color.Fuchsia;
+            this.readyLabel.Location = new System.Drawing.Point(534, 89);
+            this.readyLabel.Name = "readyLabel";
+            this.readyLabel.Size = new System.Drawing.Size(156, 23);
+            this.readyLabel.TabIndex = 1;
+            this.readyLabel.Text = "Ready to Login";
             // 
-            // label2
+            // usernameLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(480, 132);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 19);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "User Name";
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(480, 132);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(99, 19);
+            this.usernameLabel.TabIndex = 2;
+            this.usernameLabel.Text = "User Name";
             // 
             // userNameTxt
             // 
@@ -85,15 +85,15 @@
             this.passwordTxt.Size = new System.Drawing.Size(181, 22);
             this.passwordTxt.TabIndex = 5;
             // 
-            // label3
+            // passwordLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(480, 206);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 19);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Password";
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(480, 206);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(87, 19);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Password";
             // 
             // signInTxt
             // 
@@ -116,10 +116,10 @@
             this.ClientSize = new System.Drawing.Size(800, 494);
             this.Controls.Add(this.signInTxt);
             this.Controls.Add(this.passwordTxt);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.passwordLabel);
             this.Controls.Add(this.userNameTxt);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.readyLabel);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
@@ -134,11 +134,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label readyLabel;
+        private System.Windows.Forms.Label usernameLabel;
         private System.Windows.Forms.TextBox userNameTxt;
         private System.Windows.Forms.TextBox passwordTxt;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label passwordLabel;
         private System.Windows.Forms.Button signInTxt;
     }
 }
